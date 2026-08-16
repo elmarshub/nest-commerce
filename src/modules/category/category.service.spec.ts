a83052d41880a71d1999859135327e227d104b6f
elmarshub <martinsifeanyi247@gmail.com>
2026-08-16 03:03:34 +0100

    feat(tests): enhance auth and category module tests with service dependencies

--- src/modules/category/category.service.spec.ts
+++ src/modules/category/category.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '@/prisma/prisma.service';
 import { CategoryService } from './category.service';
 
 describe('CategoryService', () => {
@@ -6,7 +7,7 @@ describe('CategoryService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [CategoryService, { provide: PrismaService, useValue: {} }],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
